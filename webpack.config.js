38495becc286901bfeb5a18bb6993a84cd6d3a77
afc163 <afc163@gmail.com>
2019-01-12 16:10:16 +0800

    :bug: Fix babel preset not found issue

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         use: {
             loader: 'babel-loader'
         },
-        exclude: '/node_modules/'
+        exclude: /node_modules/,
       },
       {
         test: /\.less$/,
